0d65077be4759be0e1fed035478dc9bbd49510ce
antonio <antonio.rodriguez@glueops.dev>
2022-04-08 17:29:13 -0600

    refactor: clean up tf

--- projects.tf
+++ projects.tf
@@ -1,23 +1,26 @@
+variable "company_key" {}
+
+
+resource "google_folder" "core" {
+  display_name = "${var.company_key} Core"
+  parent       = "organizations/${var.org_id}"
+}
+
 resource "google_project" "development" {
-  name       = "${local.company_key}-development-2"
-  project_id = "${local.company_key}-development-2"
+  name       = "${var.company_key}-development"
+  project_id = "${var.company_key}-development"
   folder_id  = google_folder.core.name
 }
 
 resource "google_project" "uat" {
-  name       = "${local.company_key}-uat-2"
-  project_id = "${local.company_key}-uat-2"
+  name       = "${var.company_key}-uat"
+  project_id = "${var.company_key}-uat"
   folder_id  = google_folder.core.name
 }
 
 resource "google_project" "production" {
-  name       = "${local.company_key}-production-2"
-  project_id = "${local.company_key}-production-2"
+  name       = "${var.company_key}-production"
+  project_id = "${var.company_key}-production"
   folder_id  = google_folder.core.name
-}
-
-resource "google_folder" "core" {
-  display_name = "${local.company_key} Core"
-  parent       = "organizations/${local.org_id}"
 }
 
