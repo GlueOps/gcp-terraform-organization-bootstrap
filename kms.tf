87979643443bdb790c0160ee79fe5bb44c9a6d01
Venkata Mutyala <venkata@venkatamutyala.com>
2022-04-29 17:41:13 +0000

    fix: debugging kms

--- kms.tf
+++ kms.tf
@@ -8,7 +8,7 @@ resource "google_kms_key_ring" "keyring" {
 resource "google_kms_crypto_key" "key" {
   #for_each = local.environments
   name     = "encrypt_decrypt-${each.value.labels.environment}"
-  key_ring = google_kms_key_ring.keyring.id["development"]
+  key_ring = google_kms_key_ring.keyring["development"].id
   purpose  = "ENCRYPT_DECRYPT"
   labels   = {}
 
