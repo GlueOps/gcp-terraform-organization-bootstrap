e751c03d5f3a9f5416e262e54999a8d3917c633d
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2022-04-29 09:56:37 -0600

    fix: wrong variable

--- kms.tf
+++ kms.tf
@@ -1,11 +1,11 @@
 resource "google_kms_key_ring" "keyring" {
   project  = data.google_projects.env_project.projects[0].project_id
-  name     = var.environment
+  name     = var.workspace
   location = "global"
 }
 
 resource "google_kms_crypto_key" "key" {
-  name     = "encrypt_decrypt-${var.environment}"
+  name     = "encrypt_decrypt-${var.workspace}"
   key_ring = google_kms_key_ring.appengine.id
   purpose  = "ENCRYPT_DECRYPT"
   labels   = {}
